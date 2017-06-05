118b7e192dee3dd6f80885a7d135b87db95d5a1d
wenrui <15121985208@163.com>
2017-06-05 10:52:17 +0800

    设置生产模式配置 倒计时插件修改

--- app/js/logic.js
+++ app/js/logic.js
@@ -1,18 +1,22 @@
 import config from './config.json';
+import imgUrl from '../images/code.jpg';
 $(() => {
     //计算二维码大小和位置。已知数据：code 209*209   左上角坐标155,164   大图：750*683
     //宽占0.2787   坐标  0.2067  0.2401
-    var endTime = (new Date("2017/6/15 0:0:0")).getTime();
-    var startTime = (new Date()).getTime();
-    var needMs = endTime - startTime;
-     $('#countdown').countDown({
-		targetDate: {
-		'day': 5,
-		'month': 8,
-		'year': 2024,
-		'hour': 11,
-		'min': 0,
-		'sec': 0
-		}
-		});
+    //添加二维码omitWeeks
+    console.log(imgUrl);
+    //防止缓存
+    var stamp = new Date().getTime();
+    $('#urlCode').attr('src', imgUrl + '?t=' + stamp)
+    $('#countdown').countDown({
+        targetDate: {
+            'day': 15,
+            'month': 6,
+            'year': 2017,
+            'hour': 0,
+            'min': 0,
+            'sec': 0
+        },
+        omitWeeks: true
+    });
 })
--- webpack.pro.config.js
+++ webpack.pro.config.js
@@ -4,7 +4,7 @@ var ExtractTextPlugin = require('extract-text-webpack-plugin');
 var HtmlWebpackInlineSourcePlugin = require('html-webpack-inline-source-plugin');
 
 module.exports = {
-    devtool: 'cheap-module-eval-source-map', //配置生成Source Maps，选择合适的选项 
+    devtool: 'cheap-module-source-map', //配置生成Source Maps，选择合适的选项 
     entry: { // pagesDir是前面准备好的入口文件集合目录的路径
         'js/entry': __dirname + "/app/main.js",
         'js/vendor': ['jquery','./vendor/countdown/jquery.lwtCountdown-1.0.js']
@@ -51,26 +51,13 @@ module.exports = {
     },
     plugins: [
         new webpack.BannerPlugin("Copyright Flying Unicorns inc."), //在这个数组中new一个就可以了
-        new webpack.LoaderOptionsPlugin({
-            options: {
-                postcss: function () {
-                    return [precss, autoprefixer];
-                },
-                devServer: {
-                    contentBase: "./build", //本地服务器所加载的页面所在的目录
-                    colors: true, //终端中输出结果为彩色
-                    historyApiFallback: true, //不跳转
-                    inline: true //实时刷新
-                }
-            }
-        }),
         new HtmlWebpackPlugin({
             filename: 'index.html', //http访问路径
             template: __dirname + "/app/index.tmpl.html", //实际文件路径
             inject: true,
             chunks: ['js/entry', 'js/vendor', 'js/commons'] //new 一个这个插件的实例，并传入相关的参数
         }),
-        // new webpack.optimize.UglifyJsPlugin(), 
+        new webpack.optimize.UglifyJsPlugin(), 
         /* 抽取出所有通用的部分 */
         new webpack.optimize.CommonsChunkPlugin({
             name: 'js/commons', // 需要注意的是，chunk的name不能相同！！！
