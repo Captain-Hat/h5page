03f3be2dd670c9a46086508653d065a5a075bf00
ylb <513893049@qq.com>
2017-06-09 13:54:51 +0800

    基本组哦好了增加了动画北京

--- app/js/logic.js
+++ app/js/logic.js
@@ -1,29 +1,35 @@
-import config from './config.json';
-import imgUrl from '../images/code.jpg';
-$(() => {
-    //计算二维码大小和位置。已知数据：code 209*209   左上角坐标155,164   大图：750*683
-    //宽占0.2787   坐标  0.2067  0.2401
-    //添加二维码omitWeeks
-    console.log(imgUrl);
-    //防止缓存
-    var stamp = new Date().getTime();
-    $('#urlCode').attr('src', imgUrl + '?t=' + stamp)
-    $('#countdown').countDown({
-        targetDate: {
-            'day': 16,
-            'month': 6,
-            'year': 2017,
-            'hour': 0,
-            'min': 0,
-            'sec': 0
-        },
-        omitWeeks: true
-    });
+ import imgUrl from '../images/code.jpg';
+ import 'fullpage.js/dist/jquery.fullpage.min.js'
+ import 'fullpage.js/dist/jquery.fullpage.min.css'
+ $(() => {
+     //计算二维码大小和位置。已知数据：code 209*209   左上角坐标155,164   大图：750*683
+     //宽占0.2787   坐标  0.2067  0.2401
+     //添加二维码omitWeeks
+     console.log(imgUrl);
+     //防止缓存
+     var stamp = new Date().getTime();
+     $('.urlCode').attr('src', imgUrl + '?t=' + stamp)
+     $('#countdown').countDown({
+         targetDate: {
+             'day': 16,
+             'month': 6,
+             'year': 2017,
+             'hour': 0,
+             'min': 0,
+             'sec': 0
+         },
+         omitWeeks: true
+     });
 
-    var swiper = new Swiper('.swiper-container', {
-        pagination: '.swiper-pagination',
-        paginationClickable: true,
-        direction: 'vertical'
-    });
 
-})
+     $('#fullpage').fullpage({
+         verticalCentered: true
+     });
+     $.fn.fullpage.setAllowScrolling(true);
+     // 动态计算code-box高度
+     var totalH = $('#fullpage').height();
+     var top = $('#section0 .header').height();
+     var mid = $('#section0 .clock').height();
+     console.log(totalH + ',' + top + ',' + mid)
+     $('#section0 .code-box').height(totalH - top - mid)
+ })
--- app/main.js
+++ app/main.js
@@ -5,11 +5,10 @@ import './js/logic';
 //页面css和less
 import './css/main.css';//使用require导入css文件
 import './less/base.less';//使用require导入css文件
-import '../vendor/countdown/countdown.css';//使用require导入css文件
-import '../vendor/swiper/css/swiper.min.css';//使用require导入css文件
+import '../vendor/countdown/countdown.css';//使用require导入css文件 
 
 //不需要合并的文件搬运（需要在html引入）
 require('!!file-loader?name=static/css/[name].[ext]!../vendor/h5reset.css');
 require('!!file-loader?name=static/js/[name].[ext]!../vendor/rem.js');
-require('!!file-loader?name=static/js/[name].[ext]!jquery/dist/jquery.min.js'); 
-require('!!file-loader?name=static/js/[name].[ext]!../vendor/swiper/js/swiper.min.js'); 
+require('!!file-loader?name=static/js/[name].[ext]!jquery/dist/jquery.min.js');  
+require('!!file-loader?name=static/[name].[ext]!./flowers.mp4');  
